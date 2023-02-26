560be8d4f547f8ae325735945bfff7d8e366705c
Brahian Grajales <brahian337@gmail.com>
2023-02-26 17:57:59 -0500

    Class Date.cs created

---
+++ WebApp_OOPConcepts/WebApp_OOPConcepts/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WebApp_OOPConcepts
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("********************");
+                Console.WriteLine("*My first day class*");
+                Console.WriteLine("********************");
+                Console.WriteLine("");
+
+                Console.WriteLine("Please type your year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+
+                Console.WriteLine("Please type your month: ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+
+                Console.WriteLine("Please type your day: ");
+                int day = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+
+                var dateObject = new Date(year, month, day);
+                Console.Write("The date entered is: ");
+                Console.WriteLine(dateObject);
+            }
+            catch (Exception message)
+            {
+                Console.WriteLine(message);
+            }
+            
+        }
+    }
+}
