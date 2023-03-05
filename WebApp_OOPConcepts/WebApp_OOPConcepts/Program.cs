23925135a6c18d753cdd592d37add972538942e0
Brahian Grajales <brahian337@gmail.com>
2023-03-05 17:55:26 -0500

    New Employee.cs and SalaryEmployee.cs classes created

--- WebApp_OOPConcepts/WebApp_OOPConcepts/Program.cs
+++ WebApp_OOPConcepts/WebApp_OOPConcepts/Program.cs
@@ -28,6 +28,21 @@ namespace WebApp_OOPConcepts
                 var dateObject = new Date(year, month, day);
                 Console.Write("The date entered is: ");
                 Console.WriteLine(dateObject);
+
+                Employee salaryEmployee = new SalaryEmployee()
+                { 
+                    Id = 1,
+                    FirstName = "Patricia",
+                    LastName = "Alvarez",
+                    BirthDate = dateObject,
+                    HiringDate = dateObject,
+                    IsActive = true,
+                    Salary = 1160000
+                };
+
+                Console.WriteLine(salaryEmployee.ToString());
+
+                
             }
             catch (Exception message)
             {
